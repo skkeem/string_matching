befb9a156174c939b7d738c75ebed2047df03f4b
Sung-Kook Kim <skkeem@gmail.com>
2017-10-10 16:01:23 +0900

    test for kmp

---
+++ test_kmp.py
@@ -0,0 +1,26 @@
+import unittest
+import kmp
+
+class TestKMP(unittest.TestCase):
+    k1 = kmp.KMP("ababaca")
+    k2 = kmp.KMP("aabbaab")
+
+    def test_p(self):
+        self.assertEqual(self.k1.p, "ababaca")
+        self.assertEqual(self.k2.p, "aabbaab")
+
+    def test_ff_construction(self):
+        self.assertEqual(self.k1.ff, [0,0,1,2,3,0,1])
+        self.assertEqual(self.k2.ff, [0,1,0,0,1,2,3])
+
+    def test_search(self):
+        a = [0, 4]
+        k = 0
+        for i in self.k2.search("aabbaa"):
+            raise Exception
+        for i in self.k2.search("aabbaab"):
+            self.assertEqual(i, a[k])
+            k = k+1
+
+if __name__ == '__main__':
+    unittest.main()
