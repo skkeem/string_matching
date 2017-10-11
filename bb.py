30bb1585cf89da5e0e8c48fcc742f099f68a04d0
Sung-Kook Kim <skkeem@gmail.com>
2017-10-11 15:14:48 +0900

    added baker-bird

--- bb.py
+++ bb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from ac import AC
+from kmp import KMP
 
 class BB:
     """Class for Baker-Bird algorithm"""
@@ -8,11 +9,11 @@ class BB:
         # p : m by m sized pattern.
         self.p = list(p)
         self.m = len(p)
-        # ac : Aho-Corasick instance for p
-        ac = AC(p)
         # dr : pi[1..m] -> distinct row number.
         # pp : p converted in to sequence of distinct row #s.
         self.dr, self.pp = self.construct()
+        # ac : Aho-Corasick instance for p
+        self.ac = AC(self.dr.keys())
         # R : distinct row #[1..n, 1..n] for match.
         # !!this is constructed row by row to reduce space usage.
 
@@ -29,16 +30,25 @@ class BB:
 
     def search(self, t):
         n = len(t)
-        assert (m <= n)
+        assert (self.m <= n)
 
         ac = self.ac
+        dr = self.dr
+        kmp = KMP(self.pp)
+        nkmp = [kmp.stream() for i in range(n)]
+        for k in nkmp:
+            next(k)
         for i in range(n):
-            # r : current row of R matrix
-            r = [0 for i in range(n)]
-            # ac returns (index, output)
+            # r : current row of R matrix.
+            r = [0 for x in range(n)]
+            # ac returns (index, output).
             for j, s in ac.search(t[i]):
-                r[j] = dr[s.pop()]
+                r[j] = dr[(list(s)[0])]
+            # single step n KMPs.
+            for j in range(n):
+                if nkmp[j].send(r[j]):
+                    yield (i, j)
 
     def printAll(self, t):
-        for i in self.search(t):
-            print(i)
+        for p in self.search(t):
+            print(p)
