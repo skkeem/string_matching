b1be0c06b74c730bebce8ffbef8ecc8873acba36
Sung-Kook Kim <skkeem@gmail.com>
2017-10-12 16:35:48 +0900

    final

---
+++ README.txt
@@ -0,0 +1,9 @@
+실행 환경
+- 머신 : x86_64
+- 운영체제 : Linux 4.10.0-33-generic
+- 구현언어 : Python 3.5.2
+- 실행방법 :
+    1) python3 hw1.py <input> <output>
+    2) python3 checker.py <input> <answer> <output>
+- 각 모듈 테스트 방법 :
+    python3 -m unittest test_<모듈이름>.py
--- checker.py
+++ checker.py
@@ -2,7 +2,9 @@ import sys
 import io
 import re
 
-from bb import BB
+class Unmatch(Exception):
+    """Exception for unmatch"""
+    pass
 
 def check(buf, answer):
     # process the input
@@ -25,21 +27,31 @@ def check(buf, answer):
         j = int(pair.group(2))
         ans = ans | set([(i, j)])
 
-    # run Baker-Bird
-    bb = BB(p)
+    # run brute-force 2-D pattern matching
     computed = set()
-    for p in bb.search(t):
-        computed = computed | set([p])
+    for i in range(m-1, n):
+        for j in range(m-1, n):
+            # try matching the pattern
+            try:
+                for x in range(0, m):
+                    # global index for x
+                    xg = i-m+1+x
+                    for y in range(0, m):
+                        # global index for y
+                        yg = j-m+1+y
+                        if t[xg][yg] != p[x][y]:
+                            raise Unmatch
+                computed = computed | set([(i,j)])
+            except Unmatch:
+                pass
 
-    buf.close()
-    answer.close()
-
+    # check whether ans & computed are the same
     if ans == computed:
-        return "yes\n"
+        return "yes"
     else:
-        return "no\n"
-    
-    
+        return "no"
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage : python checker.py <input> <answer> <output>")
@@ -67,6 +79,8 @@ if __name__ == "__main__":
 
     # check the answer
     output = check(buf, answer)
+    buf.close()
+    answer.close()
 
     # write output files
     try:
